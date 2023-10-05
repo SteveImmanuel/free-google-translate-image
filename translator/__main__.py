081aa5cdb96f95cb727e00976bf02197b8a29858
SteveImmanuel <steveandreasimanuel@gmail.com>
2023-10-05 18:06:10 +0900

    Add main

---
+++ translator/__main__.py
@@ -0,0 +1,16 @@
+import sys
+
+from PyQt6.QtWidgets import QApplication
+
+from translator.gui.main_window import MainWindow
+
+
+def main():
+    app = QApplication(sys.argv)
+    win = MainWindow()
+    win.show()
+    sys.exit(app.exec())
+
+
+if __name__ == '__main__':
+    main()

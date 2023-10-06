3f642bc3833244aa69301b37cd19737df203ad51
SteveImmanuel <steveandreasimanuel@gmail.com>
2023-10-06 11:30:45 +0900

    Move translation logic to main window

--- translator/gui/main_window.py
+++ translator/gui/main_window.py
@@ -1,27 +1,28 @@
-from multiprocessing import cpu_count
 import typing
-from PyQt6 import QtGui, QtCore
 
+from PyQt6 import QtCore, QtGui
 from PyQt6.QtCore import QThreadPool
-from PyQt6.QtWidgets import QMainWindow, QPushButton, QVBoxLayout, QApplication, QWidget
+from PyQt6.QtWidgets import (QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget)
 
-from translator.gui.widgets import *
+from translator.engine import translate_emulate
+from translator.gui.const import MAX_THREADS
 from translator.gui.screenshot_window import Screenshot
+from translator.gui.widgets import *
+from translator.gui.worker import TranslateWorker
 
 
 class MainWindow(QMainWindow):
 
     def __init__(self):
         super().__init__()
-        max_thread_count = max(1, min(cpu_count() - 1, 4))
-        QThreadPool.globalInstance().setMaxThreadCount(max_thread_count)
+        QThreadPool.globalInstance().setMaxThreadCount(MAX_THREADS)
         self.last_x = None
         self.last_y = None
+        self.ss_window = None
         self._setup_ui()
 
     def _setup_ui(self):
         self.setWindowTitle('Instant Screen Translator')
-        # self.setMinimumSize(500, 650)
 
         self._lang_selector = LangSelector()
         self._btn_ss = QPushButton('Take Screenshot', self)
@@ -36,13 +37,31 @@ class MainWindow(QMainWindow):
         self._main_widget = QWidget()
         self._main_widget.setLayout(self._layout)
         self.setCentralWidget(self._main_widget)
-        # self.setLayout(self._layout)
-        # self.setCentralWidget(self._lang_selector)
+
+    def on_translate_ss(self, input_path: str):
+        worker = TranslateWorker(lambda: translate_emulate(
+            input_path,
+            'out.png',
+            self._lang_selector.source_lang,
+            self._lang_selector.target_lang,
+        ))
+        worker.signal.success.connect(self.on_translate_complete)
+        worker.signal.error.connect(self.on_translate_error)
+        print('starting worker')
+
+        QThreadPool.globalInstance().start(worker)
+
+    def on_translate_complete(self, translated_path: str, translated_text: str):
+        print(translated_path, translated_text)
+
+    def on_translate_error(self, error: str):
+        print(error)
 
     def show_ss_window(self, a):
         self.last_x = self.x()
         self.last_y = self.y()
-        self.ss_window = Screenshot(self)
+        if self.ss_window is None:
+            self.ss_window = Screenshot(self)
         self.ss_window.show()
         self.hide()
 
